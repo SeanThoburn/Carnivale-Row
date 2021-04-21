d05a69d2dd58c93e3185a1a0e59759cc1bd828f9
LucindaTemple <74675860+LucindaTemple@users.noreply.github.com>
2021-04-21 13:53:51 +0100

    Maze mechanics

---
+++ Assets/Haunted House/escape.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class escape : MonoBehaviour
+{
+    public GameObject jackBox;
+    public GameObject self;
+    public GameObject text;
+
+  private void OnTriggerEnter()
+    {
+        jackBox.gameObject.SetActive(true);
+        text.gameObject.SetActive(true);
+        self.gameObject.SetActive(false);
+    }
+}
