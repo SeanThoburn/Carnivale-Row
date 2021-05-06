64eb25331580b5e44f7088c36b9475d083b0ddc1
LucindaTemple <74675860+LucindaTemple@users.noreply.github.com>
2021-05-06 22:45:10 +0100

    Hubbo
    Adds a main menu, adds in the doors for level selection

---
+++ Assets/Mouse.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour
+{
+    public void mouse()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
