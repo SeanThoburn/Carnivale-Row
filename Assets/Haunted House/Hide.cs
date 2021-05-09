9338e6f9ea8bba982a39b33326b91a876a8157c1
LucindaTemple <74675860+LucindaTemple@users.noreply.github.com>
2021-05-09 16:19:15 +0100

    Maze Spookie
    Adds in the spookies to the maze

---
+++ Assets/Haunted House/Hide.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hide : MonoBehaviour
+{
+    public GameObject self;
+    public float time;
+    public float timer;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > time)
+        {
+            self.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Haunted House/Spook.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spook : MonoBehaviour
+{
+    public GameObject spooky;
+    public GameObject self;
+
+    private void OnTriggerEnter()
+    {
+        spooky.gameObject.SetActive(true);
+        self.gameObject.SetActive(false);
+    }
+}
