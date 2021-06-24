688a832b9a7cebfeb937099ad6f548e51998995f
LucindaTemple <74675860+LucindaTemple@users.noreply.github.com>
2021-06-24 14:16:51 +0100

    Events Update
    Updating the events in the maze and the boxes aswell as pacing for the maze

--- Assets/Haunted House/escape.cs
+++ Assets/Haunted House/escape.cs
@@ -7,11 +7,15 @@ public class escape : MonoBehaviour
     public GameObject jackBox;
     public GameObject self;
     public GameObject text;
+    public GameObject countdown;
+    public GameObject timer;
 
-  private void OnTriggerEnter()
+    private void OnTriggerEnter()
     {
         jackBox.gameObject.SetActive(true);
         text.gameObject.SetActive(true);
+        countdown.gameObject.SetActive(true);
+        timer.gameObject.SetActive(true);
         self.gameObject.SetActive(false);
     }
 }
